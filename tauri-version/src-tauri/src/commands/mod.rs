53a1d086c2089c6f9b4f07b325477c418c029a22
sfhzyq <51251878+sfhzyq@users.noreply.github.com>
2026-09-19 14:18:05 +0800

    Delete tauri-version directory

--- tauri-version/src-tauri/src/commands/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod auth;
-pub mod credentials;
-pub mod sap;
-pub mod transfer;
-pub mod settings;
--- tauri-version/src-tauri/src/main.rs
+++
@@ -1,5 +0,0 @@
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-
-fn main() {
-    sap_login_manager_lib::run()
-}
